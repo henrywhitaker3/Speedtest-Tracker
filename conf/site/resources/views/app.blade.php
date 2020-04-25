478c5eff315c82f77e0407b61b8dca2b225eae4d
Henry Whitaker <henrywhitaker3@outlook.com>
2020-04-25 12:31:41 +0100

    Updated to v1.2.8
    Added favicons

--- conf/site/resources/views/app.blade.php
+++ conf/site/resources/views/app.blade.php
@@ -11,6 +11,24 @@
         <link rel="stylesheet" href="/css/bootstrap.dark.min.css">
         <link rel="stylesheet" href="/css/main.css">
 
+        <link rel="apple-touch-icon" sizes="57x57" href="/icons/fav/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/fav/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/icons/fav/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/fav/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/icons/fav/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/fav/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/icons/fav/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/fav/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/fav/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/icons/fav/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/fav/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/fav/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/fav/favicon-16x16.png">
+        <link rel="manifest" href="/icons/fav/manifest.json">
+        <meta name="msapplication-TileColor" content="#303030">
+        <meta name="msapplication-TileImage" content="/icons/fav/ms-icon-144x144.png">
+        <meta name="theme-color" content="#303030">
+
         <title>{{ $title }}</title>
     </head>
     <body>
