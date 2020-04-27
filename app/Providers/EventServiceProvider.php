fc35da9f8d7ec2bc0505d87df0a1088898a777bd
Henry Whitaker <henrywhitaker3@outlook.com>
2020-04-27 18:50:01 +0100

    Added slack notifications

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\SpeedtestCompleteEvent;
+use App\Listeners\SpeedtestCompleteListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,6 +19,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        SpeedtestCompleteEvent::class => [
+            SpeedtestCompleteListener::class,
         ],
     ];
 
