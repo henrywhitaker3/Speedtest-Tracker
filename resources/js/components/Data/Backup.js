4904076f1e0c9fcca2cdeca05b3509948f48816f
Henry Whitaker <henrywhitaker3@outlook.com>
2020-06-10 12:49:05 +0100

    Added front-end for csv/json backup

--- resources/js/components/Data/Backup.js
+++ resources/js/components/Data/Backup.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import Axios from 'axios';
 
 export default class Backup extends Component {
-    backup = () => {
-        var url = 'api/backup';
+    backup = (format) => {
+        var url = 'api/backup?format=' + format;
 
         toast.info('Your backup has started downloading...');
 
@@ -29,11 +29,16 @@ export default class Backup extends Component {
 
     render() {
         return (
-            <Button
-                variant="primary"
-                className="mx-2"
-                onClick={this.backup}
-            >Backup</Button>
+            <Dropdown className="m-2">
+                <Dropdown.Toggle variant="primary" id="backupDropdown">
+                    Backup
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#" onClick={() => { this.backup('json') }}>JSON</Dropdown.Item>
+                    <Dropdown.Item href="#" onClick={() => { this.backup('csv') }}>CSV</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
         );
     }
 }
