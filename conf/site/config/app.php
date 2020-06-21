6e33cb5e14205f0139f603f3d178c0a68c612c3f
Henry Whitaker <henrywhitaker3@outlook.com>
2020-06-21 14:11:23 +0100

    Added updated site files

--- conf/site/config/app.php
+++ conf/site/config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        NotificationChannels\Telegram\TelegramServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
