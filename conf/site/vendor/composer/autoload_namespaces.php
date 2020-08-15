2e68abf665f2c0e11ba29cd775f859d79bda725c
Henry Whitaker <henrywhitaker3@outlook.com>
2020-08-15 17:32:47 +0100

    composer and node

--- conf/site/vendor/composer/autoload_namespaces.php
+++ conf/site/vendor/composer/autoload_namespaces.php
@@ -9,4 +9,5 @@ return array(
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
     'Highlight\\' => array($vendorDir . '/scrivo/highlight.php'),
     'HighlightUtilities\\' => array($vendorDir . '/scrivo/highlight.php'),
+    'Barryvdh' => array($vendorDir . '/barryvdh/reflection-docblock/src'),
 );
