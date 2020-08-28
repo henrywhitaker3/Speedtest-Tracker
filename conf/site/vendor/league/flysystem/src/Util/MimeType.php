fde92f60fe4067f2dd1f6c7bfaec7c9f8c64ded4
Henry Whitaker <henrywhitaker3@outlook.com>
2020-08-28 20:42:05 +0100

    composer and npm

--- conf/site/vendor/league/flysystem/src/Util/MimeType.php
+++ conf/site/vendor/league/flysystem/src/Util/MimeType.php
@@ -37,7 +37,7 @@ class MimeType
      *
      * @param mixed $content
      *
-     * @return string|null MIME Type or NULL if no mime type detected
+     * @return string MIME Type
      */
     public static function detectByContent($content)
     {
@@ -45,7 +45,7 @@ class MimeType
             return static::detector()->detectMimeTypeFromBuffer($content);
         }
 
-        return null;
+        return 'text/plain';
     }
 
     /**
@@ -53,7 +53,7 @@ class MimeType
      *
      * @param string $extension
      *
-     * @return string|null MIME Type or NULL if no extension detected
+     * @return string MIME Type
      */
     public static function detectByFileExtension($extension)
     {
@@ -63,11 +63,11 @@ class MimeType
     /**
      * @param string $filename
      *
-     * @return string|null MIME Type or NULL if no extension detected
+     * @return string MIME Type
      */
     public static function detectByFilename($filename)
     {
-        return static::detector()->detectMimeTypeFromPath($filename);
+        return static::detector()->detectMimeTypeFromPath($filename) ?: 'text/plain';
     }
 
     /**
