0b8ccfd8d34ab624c96715308fd16140957f70bf
Henry Whitaker <henrywhitaker3@outlook.com>
2021-04-10 22:27:31 +0100

    Added speedtest seeder

---
+++ database/factories/SpeedtestFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Speedtest;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SpeedtestFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Speedtest::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'download' => rand(15, 900),
+            'upload' => rand(15, 900),
+            'ping' => rand(1, 25),
+            'scheduled' => (bool) rand(0, 1),
+        ];
+    }
+}
