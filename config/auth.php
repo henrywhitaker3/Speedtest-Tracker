6905a5aac0111738186cd13d48590fdc6a6466dd
Henry Whitaker <henrywhitaker3@outlook.com>
2021-09-11 09:29:21 +0100

    Fix bug where failure graph would not update

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [
